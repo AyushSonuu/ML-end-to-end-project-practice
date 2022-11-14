c8c2e8403d88be54fe7ff7fde2fc24e11a836f15
ayush <sonuayush55@gmail.com>
2022-11-14 11:19:55 +0530

    html added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_templete
 
 app = Flask(__name__)
 
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route("/",methods=['GET','POST'])
 def index():
     return "starting ml project"
+
+@app.route("/x",methods=['GET','POST'])
+def x():
+    return render_templete(index.html)
 
 if __name__ == "__main__":
     app.run(debug = True)
