b54eed6cb36b77235f75ce0ccca1a02ecb2b0917
Ayush <111406614+AyushSonuu@users.noreply.github.com>
2022-11-14 16:53:01 +0530

    updated bro

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "starting ml project"
+    return "hi amit bhai how are you"
 
 if __name__ == "__main__":
     app.run(debug = True)
