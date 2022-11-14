e851a5f9665700dde10b9f14bd51332cd89aeccc
ayush <sonuayush55@gmail.com>
2022-11-14 11:51:29 +0530

    cicd done

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return render_template("index.html")
-
-# @app.route("/x",methods=['GET','POST'])
-# def x():
-#     return render_templete("index.html")
+    return "starting ml project"
 
 if __name__ == "__main__":
     app.run(debug = True)
