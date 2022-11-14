38d643ff45bd5d5bf853f2646ba93a67b7bedad8
ayush <sonuayush55@gmail.com>
2022-11-14 11:23:56 +0530

    test

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "starting ml project"
-
-@app.route("/x",methods=['GET','POST'])
-def x():
     return render_templete("index.html")
+
+# @app.route("/x",methods=['GET','POST'])
+# def x():
+#     return render_templete("index.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
