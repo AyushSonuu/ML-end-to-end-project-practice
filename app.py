67bb9d890d2679749649a1347f5ada13bc886b2c
ayush <sonuayush55@gmail.com>
2022-11-16 19:27:45 +0530

    fin

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "hi amit bhai how are you"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
