9582f341c747ead20e36c4fb97c011bc8a7435a3
ayush <sonuayush55@gmail.com>
2022-11-19 07:01:50 +0530

    logger and exception done

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
-
+from housing.logger import logging
+import sys
+from housing.exception import HousingException
 app = Flask(__name__)
 
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    try:
+        raise Exception("we are testing custom exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_messsage)
+        logging.info("we are testing logging module")
     return render_template("index.html")
 
 if __name__ == "__main__":
---
+++ housing/exception/__init__.py
@@ -0,0 +1,31 @@
+import os 
+import sys
+
+
+class HousingException(Exception):
+
+    def __init__(self,error_message:Exception,error_details:sys):
+        super().__init__(error_message)
+        self.error_messsage = HousingException.get_detailed_error_message(error_message=error_message,
+                                                                          error_details=error_details)
+        # self.error_details = error_details
+
+    @staticmethod
+    def get_detailed_error_message(error_message:Exception,error_details:sys)->str:
+        '''error_mesage: Exceptio object
+           error_details: object of sys module
+        '''
+        _,_ , exec_tb = error_details.exc_info()
+        
+        line_number = exec_tb.tb_frame.f_lineno
+        file_name = exec_tb.tb_frame.f_code.co_filename
+        error_message = f"Error occured in script: [{file_name}]\nat line number: [{line_number}]\nerror_message: [{error_message}]"
+
+        return error_message
+
+    def __str__(self):
+        return self.error_messsage
+
+    def __repr__(self) -> str:
+        return HousingException.__name__.str()
+    
